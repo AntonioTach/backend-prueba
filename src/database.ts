d98c65517d2a0fd01be3f65615c6b9c06497efcf
AntonioTach <antach.vihe@gmail.com>
2023-06-04 18:24:14 -0600

    Configuration: Server and db in prod

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,20 @@
 import { createPool } from 'mysql2/promise'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
 
 //Database configuration
 export async function connect() {
     const connection = await createPool({
-        host: 'localhost',
-        user: 'root',
-        database: 'user_management',
-        connectionLimit: 100
+      host: dbHost,
+      user: dbUser,
+      password: dbPassword,
+      database: dbName,
+      connectionLimit: 100
     });
     return connection;
-}
+  }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { App } from './app';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT;
 
 async function main() {
-    const app = new App(3000);
+    const app = new App(PORT);
     await app.listen();
 }
 
